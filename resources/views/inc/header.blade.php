75a4d450b8b8a369a25f546bc1c6fdf0a683894c
Yurii <yura93lobur@gmail.com>
2021-11-09 22:23:48 +0200

    contact form

--- resources/views/inc/header.blade.php
+++ resources/views/inc/header.blade.php
@@ -2,9 +2,9 @@
     <div class="container">
         <div class="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start">
             <ul class="nav col-12 col-lg-auto me-lg-auto mb-2 justify-content-center mb-md-0">
-                <li><a href="/" class="nav-link px-2 text-white">Home</a></li>
-                <li><a href="/about" class="nav-link px-2 text-white">About us</a></li>
-                <li><a href="/contacts" class="nav-link px-2 text-white">Contacts</a></li>
+                <li><a href="{{ route('home') }}" class="nav-link px-2 text-white">Home</a></li>
+                <li><a href="{{ route('about') }}" class="nav-link px-2 text-white">About us</a></li>
+                <li><a href="{{ route('contacts') }}" class="nav-link px-2 text-white">Contacts</a></li>
             </ul>
 
             <form class="col-12 col-lg-auto mb-3 mb-lg-0 me-lg-3">
--- routes/web.php
+++ routes/web.php
@@ -4,12 +4,16 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('home');
-});
+})->name('home');
 
 Route::get('/about', function () {
     return view('about');
-});
+})->name('about');
 
 Route::get('/contacts', function () {
     return view('contacts');
-});
+})->name('contacts');
+
+Route::post('/contacts/submit', function () {
+    dd(Request::all());
+})->name('contact-form');
