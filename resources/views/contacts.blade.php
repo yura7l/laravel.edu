e00035513ecc921ca2b74073aa2942679857c1eb
Yurii <yura93lobur@gmail.com>
2021-11-10 23:17:18 +0200

    minor changes

--- resources/views/contacts.blade.php
+++ resources/views/contacts.blade.php
@@ -11,15 +11,15 @@
             @csrf
             <div class="mb-3">
                 <label for="name" class="form-label">Name</label>
-                <input type="text" class="form-control" id="name" name="name" placeholder="Your name">
+                <input type="text" class="form-control" id="name" name="name" placeholder="Your name" required>
             </div>
             <div class="mb-3">
                 <label for="email" class="form-label">Email</label>
-                <input type="email" class="form-control" id="email" name="email" placeholder="Your email">
+                <input type="email" class="form-control" id="email" name="email" placeholder="Your email" required>
             </div>
             <div class="mb-3">
                 <label for="message" class="form-label">Message</label>
-                <textarea class="form-control" id="message" name="message" placeholder="Leave your message"></textarea>
+                <textarea class="form-control" id="message" name="message" placeholder="Leave your message" required></textarea>
             </div>
             <button type="submit" class="btn btn-primary">Submit</button>
         </form>
--- resources/views/inc/hero.blade.php
+++ resources/views/inc/hero.blade.php
@@ -1,4 +1,4 @@
-<div class="px-4 py-5 my-5 text-center">
+<div class="px-4 py-5 my-5 rounded-3 border shadow-lg text-center">
     <h1 class="display-5 fw-bold">Welcome</h1>
     <div class="col-lg-6 mx-auto">
         <p class="lead mb-4">It's my first Laravel website.</p>
