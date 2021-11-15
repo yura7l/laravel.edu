ab981aca422f613e3275a2c0d1120edaed942c04
Yurii <yura93lobur@gmail.com>
2021-11-15 22:11:52 +0200

    creating news model + table in DB using migration

---
+++ database/migrations/2021_11_15_200352_create_news_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateNewsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('news', function (Blueprint $table) {
+            $table->id();
+            $table->boolean('active');
+            $table->dateTime('active_from');
+            $table->string('name');
+            $table->text('preview_text');
+            $table->longText('detail_text');
+            $table->string('preview_image');
+            $table->string('detail_image');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('news');
+    }
+}
