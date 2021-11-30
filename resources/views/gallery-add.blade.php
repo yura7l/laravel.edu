0ee208afef563eda4796cd671e06a00381e48f74
Yurii <yura93lobur@gmail.com>
2021-11-30 23:23:34 +0200

    gallery add form

---
+++ resources/views/gallery-add.blade.php
@@ -0,0 +1,28 @@
+@extends('layouts.app')
+
+@section('title')Gallery | Laravel.edu @endsection
+
+@section('content')
+    <div class="gallery">
+        <h1>Add a photo</h1>
+        <div class="gallery__form">
+            <form action="{{ route('news-add-submit') }}" method="post" enctype="multipart/form-data">
+                @csrf
+                <div class="mb-3">
+                    <label for="source" class="form-label">Photo</label>
+                    <input class="form-control" type="file" id="source" name="source">
+                </div>
+                <div class="mb-3">
+                    <label for="caption" class="form-label">Caption</label>
+                    <input type="text" class="form-control" id="caption" name="caption">
+                </div>
+
+                <button type="submit" class="btn btn-primary">Save</button>
+            </form>
+        </div>
+    </div>
+@endsection
+
+@section('aside')
+    @parent
+@endsection
